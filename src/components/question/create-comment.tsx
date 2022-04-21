92b35d321562be9110f1851d4a4d2230c3851f7f
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-04-21 23:08:47 +0430

    more extra condition to showing create comment form

--- src/components/question/create-comment.tsx
+++ src/components/question/create-comment.tsx
@@ -11,48 +11,53 @@ const CreateComment: FC = () => {
 
   return (
     <>
-      {form && !loading.loadings[Lists.COMMENTS] && loading.loadings[Lists.COMMENTS] !== undefined && (
-        <div dir="rtl">
-          <div>
-            <h3 className="text-xl font-bold">پاسخ خود را ثبت کنید</h3>
-          </div>
-
-          <form
-            onSubmit={event => {
-              event.preventDefault();
-              onSubmit(Forms.CREATE_NEW_COMMENT);
-            }}
-          >
+      {form &&
+        !loading.loadings[Lists.COMMENTS] &&
+        loading.loadings[Lists.COMMENTS] !== undefined &&
+        Object.keys(loading.errors).length === 0 && (
+          <div dir="rtl">
             <div>
-              <div className="mb-6 text-right" dir="rtl">
-                <label dir="rtl" className="text-right text-xs font-bold mb-2" htmlFor="replay">
-                  پاسخ خود را بنویسید
-                </label>
-                <textarea
-                  className="border-solid border-2 border-gray-100 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="replay"
-                  rows={10}
-                  value={form[Inputs.COMMENT].value}
-                  onChange={event => {
-                    onChange(Forms.CREATE_NEW_COMMENT, Inputs.COMMENT, event.target.value);
-                  }}
-                ></textarea>
-                <p className="text-red-500 text-xs italic">{form[Inputs.COMMENT].error}</p>
+              <h3 className="text-xl font-bold">پاسخ خود را ثبت کنید</h3>
+            </div>
+
+            <form
+              onSubmit={event => {
+                event.preventDefault();
+                onSubmit(Forms.CREATE_NEW_COMMENT);
+              }}
+            >
+              <div>
+                <div className="mb-6 text-right" dir="rtl">
+                  <label dir="rtl" className="text-right text-xs font-bold mb-2" htmlFor="replay">
+                    پاسخ خود را بنویسید
+                  </label>
+                  <textarea
+                    className="border-solid border-2 border-gray-100 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="replay"
+                    rows={10}
+                    value={form[Inputs.COMMENT].value}
+                    onChange={event => {
+                      onChange(Forms.CREATE_NEW_COMMENT, Inputs.COMMENT, event.target.value);
+                    }}
+                  ></textarea>
+                  <p className="text-red-500 text-xs italic">{form[Inputs.COMMENT].error}</p>
+                </div>
               </div>
-            </div>
 
-            <div>
-              <button
-                disabled={!forms.formValidation[Forms.CREATE_NEW_COMMENT] || loading.loadings[Forms.CREATE_NEW_COMMENT]}
-                type="submit"
-                className="primary-btn text-white text-xs text-xl font-bold py-2 px-20 rounded"
-              >
-                ارسال پاسخ
-              </button>
-            </div>
-          </form>
-        </div>
-      )}
+              <div>
+                <button
+                  disabled={
+                    !forms.formValidation[Forms.CREATE_NEW_COMMENT] || loading.loadings[Forms.CREATE_NEW_COMMENT]
+                  }
+                  type="submit"
+                  className="primary-btn text-white text-xs text-xl font-bold py-2 px-20 rounded"
+                >
+                  ارسال پاسخ
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
     </>
   );
 };
