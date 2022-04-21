c91cb54552b913c2d3fb4f971fc45f0523c2bb37
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-04-21 15:06:29 +0430

    fixing invalid import

--- src/store/actions/root-actions.ts
+++ src/store/actions/root-actions.ts
@@ -1,7 +1,6 @@
 import { Actions as FormActions } from './forms/types';
-import { Actions as QuestionDetailsActions } from './question-details/types';
 import { Actions as QuestionsActions } from './lists/types';
 import { Actions as ModalActions } from './modal/types';
 import { Actions as LoadingActions } from './loading/types';
 
-export type RootActions = FormActions | QuestionDetailsActions | QuestionsActions | ModalActions | LoadingActions;
+export type RootActions = FormActions | QuestionsActions | ModalActions | LoadingActions;
