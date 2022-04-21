65e06b49ca34d4a4c9d2fe20f4da4b3b4ccc3108
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-04-21 21:33:08 +0430

    sorting lists

--- src/apis/lists.ts
+++ src/apis/lists.ts
@@ -4,14 +4,14 @@ import { Lists } from '../store/reducers/lists/types';
 export const listsApi: { [key: string]: (...args: any[]) => ReqInput<any> } = {
   [Lists.QUESTIONS]() {
     return {
-      url: '/questions?_embed=comments',
+      url: '/questions?_embed=comments&_sort=createdAt&_order=desc',
       method: 'GET',
     };
   },
 
   [Lists.COMMENTS](data) {
     return {
-      url: `/comments?questionId=${data.questionId}`,
+      url: `/comments?questionId=${data.questionId}&_sort=createdAt&_order=desc`,
       method: 'GET',
     };
   },
